0af787e9263ab346671d3da4bfd09dade94ef554
William Quayson <wgsquayson@gmail.com>
2024-09-17 10:55:02 -0300

    add index file to ui folders, add layout component

---
+++ src/ui/components/layout/index.tsx
@@ -0,0 +1,25 @@
+import { PropsWithChildren } from "react";
+import useStyle from "../../hooks/use-style";
+import { KeyboardAvoidingView, Platform, SafeAreaView } from "react-native";
+
+export default function Layout({ children }: PropsWithChildren) {
+  const styles = useStyle((theme) => ({
+    container: {
+      width: "100%",
+      height: "100%",
+      backgroundColor: theme.colors.background.primary,
+      paddingTop: 56,
+      paddingHorizontal: 16,
+      paddingBottom: 16,
+    },
+  }));
+
+  return (
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      style={styles.container}
+    >
+      <SafeAreaView>{children}</SafeAreaView>
+    </KeyboardAvoidingView>
+  );
+}

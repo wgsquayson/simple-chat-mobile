90adaf8105d655d738c308c11bea326d3e7764fb
William Quayson <wgsquayson@gmail.com>
2024-09-17 10:58:26 -0300

    add spacing and fontsizes values to default theme

--- src/ui/themes.ts
+++ src/ui/themes.ts
@@ -7,6 +7,23 @@ export const defaultTheme = {
       primary: "#FFFFFF",
     },
   },
+  spacing: {
+    xxxs: 4,
+    xxs: 8,
+    xs: 12,
+    sml: 16,
+    md: 24,
+    lg: 32,
+    xl: 40,
+    xxl: 56,
+  },
+  fontSizes: {
+    xs: 12,
+    sml: 16,
+    md: 24,
+    lg: 32,
+    xl: 40,
+  },
 };
 
 export type Theme = typeof defaultTheme;

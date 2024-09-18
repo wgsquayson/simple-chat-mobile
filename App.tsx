45bee47a050f5db1742a63c2e0c9580304c9ad4d
William Quayson <wgsquayson@gmail.com>
2024-09-18 17:11:55 -0300

    setup google sign in

--- App.tsx
+++ App.tsx
@@ -1,6 +1,9 @@
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import Routes from "./src/routes";
 import { StatusBar } from "expo-status-bar";
+import { GoogleSignin } from "@react-native-google-signin/google-signin";
+
+GoogleSignin.configure();
 
 export default function App() {
   return (
